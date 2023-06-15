5f3f396983d31bad20da084a5fdba51046a45745
zola2311 <zola2311@gmail.com>
2023-06-15 16:37:34 +0300

    categories model modified

--- Naiothchurch/app/Models/Member.php
+++ Naiothchurch/app/Models/Member.php
@@ -5,13 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Member extends Model
 {
-
-    protected $guarded=[];
     use HasFactory;
-    public function galleries()
-    {
-        return $this->hasMany(Gallery::class);
-    }
+    protected $guarded=[];
 }
